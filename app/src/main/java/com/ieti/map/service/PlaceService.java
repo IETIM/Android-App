a5cd97c932f95f95425e5c27073b5b02e7891a9e
JaySanchez0 <jeisson.sanchez@mail.escuelaing.edu.co>
2020-11-16 18:55:10 -0500

    Places

---
+++ app/src/main/java/com/ieti/map/service/PlaceService.java
@@ -0,0 +1,13 @@
+package com.ieti.map.service;
+
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+public interface PlaceService {
+    @GET("v1/geocode?apiKey=0aizyNbKZY4J9HTyfyGWqiIltD1mmBqxngImkcvaBS8")
+    Call<List<Place>> getPlaces(@Query("q") String name);
+}
--- app/src/main/java/com/ieti/ui/gallery/GalleryFragment.java
+++ app/src/main/java/com/ieti/ui/gallery/GalleryFragment.java
@@ -1,5 +1,6 @@
 package com.ieti.ui.gallery;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,24 +13,36 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.ieti.ui.MapsActivity;
 import com.ieti.ui.R;
 
-public class GalleryFragment extends Fragment {
+public class GalleryFragment extends Fragment implements OnMapReadyCallback {
 
-    private GalleryViewModel galleryViewModel;
+    private GoogleMap mMap;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        galleryViewModel =
-                new ViewModelProvider(this).get(GalleryViewModel.class);
         View root = inflater.inflate(R.layout.fragment_gallery, container, false);
-        final TextView textView = root.findViewById(R.id.text_gallery);
-        galleryViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
+        Intent intent = new Intent(getContext(), MapsActivity.class);
+        getActivity().onBackPressed();
+        getContext().startActivity(intent);
         return root;
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng sydney = new LatLng(-34, 151);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
 }
