ccb488f45b29a9db083c16e146110a82133954f0
JaySanchez0 <jeisson.sanchez@mail.escuelaing.edu.co>
2020-12-03 19:49:47 -0500

    url reader

--- app/src/main/java/com/ieti/ui/stores/StoreAdapter.java
+++ app/src/main/java/com/ieti/ui/stores/StoreAdapter.java
@@ -1,22 +1,31 @@
 package com.ieti.ui.stores;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.ieti.model.Shop;
 import com.ieti.ui.R;
 import com.ieti.ui.products.ProductsActivity;
 
+import java.net.URL;
 import java.util.List;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
 
 public class StoreAdapter extends BaseAdapter {
 
     private Context context;
+
+    private Executor executor = Executors.newFixedThreadPool(1);
 
     private List<Shop> shops;
 
@@ -49,6 +58,18 @@ public class StoreAdapter extends BaseAdapter {
         card.findViewById(R.id.idItem_TiendaVer).setOnClickListener((view) -> {
             redirectProducts(shop.getId());
         });
+        executor.execute(()->{
+            try{
+                URL url = new URL(shop.getImage());
+                Bitmap bmp = BitmapFactory.decodeStream(url.openConnection().getInputStream());
+                ((Activity) context).runOnUiThread(()->{
+                    ((ImageView)((Activity) context).findViewById(R.id.img)).setImageBitmap(bmp);
+                });
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+        });
+
         return card;
     }
 
